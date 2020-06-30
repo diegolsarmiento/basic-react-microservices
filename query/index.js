d17f38e7cc0283f9be96775f6d32e0956d1c10dc
Diego Sarmiento <dlsarmiento@gmail.com>
2020-06-30 18:30:24 -0400

    Updated basic logic to handle Event Bus down

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,9 +13,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated'){
         const { id, title  } = data;
 
@@ -28,7 +27,6 @@ app.post('/events', (req, res) => {
             post.comments.push({ id, content, status });
         }
     }
-
     if (type === 'CommentUpdated'){
         const { id, content, postId, status } = data;
 
@@ -40,12 +38,25 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type,data);
+
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002 - Query');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    // Whatever we use axios, res has data available
+    for(let event of res.data){
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
 
